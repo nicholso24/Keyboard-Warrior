124be68dac5461c0b66358fb8d627f817069c24a
Chase Ohmstede <aceohmstede@gmail.com>
2022-09-20 14:18:29 -0700

    working on enemy sprites
    currently have Drone sprites

--- Keyboard Warrior/Main.gd
+++ Keyboard Warrior/Main.gd
@@ -2,6 +2,10 @@ extends Node
 
 var scene = load("res://Robot.tscn")
 
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 func _on_RobotTimer_timeout():
 	var robot = scene.instance()
 	add_child(robot)
--- Keyboard Warrior/Robot.gd
+++ Keyboard Warrior/Robot.gd
@@ -1,24 +1,28 @@
 extends KinematicBody2D
 
-
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 onready var player = get_node("/root/Main/Player")
 var velocity
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$AnimatedSprite_Drone.play()
 	yield(get_tree().create_timer(1),"timeout")
 
-
-func _process(delta):
+func _physics_process(delta):
 	if player:
 		yield(get_tree().create_timer(0.25), "timeout")
 		velocity = Vector2.ZERO
 		velocity = position.direction_to(player.position) * 100
 		velocity = move_and_collide(velocity * delta)
+		if velocity != 0:
+			$AnimatedSprite_Drone.animation = "run"
+			$AnimatedSprite_Drone.flip_h = velocity.x < 0
+		else:
+			$AnimatedSprite_Drone.animation = "idle"
 		
 func kill():
+	$AnimatedSprite_Drone.animation = "death"
 	queue_free()
