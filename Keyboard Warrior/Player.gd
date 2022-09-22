61b25c47be7657bcc49045d14eabe8f3abe91097
nicholso24 <nicholso24@up.edu>
2022-09-22 15:34:11 -0700

    added start/endgame

--- Keyboard Warrior/Player.gd
+++ Keyboard Warrior/Player.gd
@@ -2,11 +2,15 @@ extends KinematicBody2D
 
 signal hit
 export (int) var speed = 500
-
+var screen_size = Vector2.ZERO
 
 onready var target = position
 var velocity = Vector2()
-
+func _ready():
+	screen_size = get_viewport_rect().size
+	print(screen_size)
+	hide()
+	
 func _input(event):
 	if event.is_action_pressed("move"):
 		target = get_global_mouse_position()
@@ -16,14 +20,25 @@ func _physics_process(delta):
 	# look_at(target)
 	if position.distance_to(target) > 5:
 		#velocity = move_and_collide(velocity)
-		var player = move_and_collide(velocity * delta)
+		var enemy = move_and_collide(velocity * delta)
+		if enemy:
+			hide()
+			$CollisionShape2D.set_deferred("disabled",true)
+			emit_signal("hit")
+			
+			
 		
-		if player:
-				hide()
-				yield(get_tree().create_timer(5), "timeout")
-				show()
+		
+				
 func kill():
 	print("HAHAHA")
+	
+func start(new_position):
+	position = new_position
+	show()
+	$CollisionShape2D.disabled = false
+	
+
 
 
 
