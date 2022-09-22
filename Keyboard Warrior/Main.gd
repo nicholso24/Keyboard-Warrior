36bf43eda213895f69cdf97b050680e341aa79b1
nicholso24 <78463373+nicholso24@users.noreply.github.com>
2022-09-22 15:51:47 -0700

    Revert "added start/endgame"

--- Keyboard Warrior/Main.gd
+++ Keyboard Warrior/Main.gd
@@ -1,25 +1,8 @@
 extends Node
 
-var score = 0
+
 var scene = load("res://Robot.tscn")
 
-func _ready():
-	pass
-func new_game():
-	score = 0
-	$StartTimer.start()
-	$HUD.show_message("Get ready gamer!")
-	$Player.start($StartPosition.position)
-	$HUD.update_score(score)
-	yield($StartTimer,"timeout")
-	$ScoreTimer.start()
-	$RobotTimer.start()
-	
-func game_over():
-	get_tree().call_group("mobs","queue_free")
-	$ScoreTimer.stop()
-	$RobotTimer.stop()
-	$HUD.show_game_over()
 	
 
 func _on_RobotTimer_timeout():
@@ -27,8 +10,3 @@ func _on_RobotTimer_timeout():
 	add_child(robot)
 	
 	robot.position = $RobotSpawn.get_position()
-
-
-func _on_ScoreTimer_timeout():
-	score += 1
-	$HUD.update_score(score)
