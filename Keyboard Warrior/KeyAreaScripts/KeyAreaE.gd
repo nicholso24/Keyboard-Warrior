c9b295a03392640c4562c48ebb8029b25fb0d1d4
Baird24 <89880795+Baird24@users.noreply.github.com>
2022-09-25 20:47:32 -0700

    Key Cooldowns working

--- Keyboard Warrior/KeyAreaScripts/KeyAreaE.gd
+++ Keyboard Warrior/KeyAreaScripts/KeyAreaE.gd
@@ -1,10 +1,20 @@
 extends Area2D
 
+var keyUsed = true
+var time = 10
+var cooldownCol = Color(.9, .0, .0, .18)
+var normalCol = Color(.08, 1.0, .0, .18)
 
 func _process(delta):
 	var bodies = get_overlapping_bodies()
 #	if bodies.size() > 0:
 #		print(bodies)
-	if Input.is_action_just_pressed("e_key"):
+	if Input.is_action_just_pressed("e_key") and keyUsed:
 		for node in bodies:
 			node.kill()
+		keyUsed = false
+		get_node("/root/Main/ColorRect/KeyE").color = cooldownCol
+		yield(get_tree().create_timer(time), "timeout")
+		get_node("/root/Main/ColorRect/KeyE").color = normalCol
+		print("Key ready")
+		keyUsed = true
