a5d98e00bf7d3efa0238030a017d60c16cccdc53
cianpm01 <murrayc24@up.edu>
2022-10-01 14:59:42 -0700

    Added Start Screen

--- Keyboard Warrior/Main.gd
+++ Keyboard Warrior/Main.gd
@@ -2,6 +2,8 @@ extends Node
 
 var score = 0
 var scene = load("res://Robot.tscn")
+var term = load("res://Terminator.tscn")
+var unstop = load("res://Unstoppable.tscn")
 func _ready():
 	randomize()
 
@@ -18,6 +20,8 @@ func new_game():
 	yield($StartTimer,"timeout")
 	$ScoreTimer.start()
 	$RobotTimer.start()
+	$TermTimer.start()
+	$UnstopTimer.start()
 	
 	
 
@@ -26,7 +30,9 @@ func game_over():
 	get_tree().call_group("mobs","queue_free")
 	$ScoreTimer.stop()
 	$RobotTimer.stop()
+	$TermTimer.stop()
 	$HUD.show_game_over()
+	$UnstopTimer.stop()
 	
 
 func _on_RobotTimer_timeout():
@@ -44,3 +50,33 @@ func _on_RobotTimer_timeout():
 func _on_ScoreTimer_timeout():
 	score += 1
 	$HUD.update_score(score)
+
+
+func _on_TermTimer_timeout():
+	var mob_spawn_location = $MobPath/MobSpawn
+	mob_spawn_location.unit_offset = randf()
+	
+	var robot = term.instance()
+	add_child(robot)
+	
+	robot.position = mob_spawn_location.position
+	
+
+
+func _on_UnstopTimer_timeout():
+	var dir = true
+	if randi() % 2:
+		dir = false
+	if dir:
+		var mob_spawn_location = $UnstopPosition1
+		var robot = unstop.instance()
+		add_child(robot)
+		robot.position = mob_spawn_location.position
+		robot._on_spawn($UnstopPosition2)
+		
+	else:
+		var mob_spawn_location = $UnstopPosition2
+		var robot = unstop.instance()
+		add_child(robot)
+		robot.position = mob_spawn_location.position
+		robot._on_spawn($UnstopPosition1)
